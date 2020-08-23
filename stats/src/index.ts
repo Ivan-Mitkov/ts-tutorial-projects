1348e2289bda2851f3c9999422fa02c0d6c5684f
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-08-23 11:01:44 +0300

    parsing data

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi");
+import fs from "fs";
+
+const maches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((x): string[] => x.split(","));
+// console.log(maches);
